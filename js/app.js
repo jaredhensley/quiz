da211f3d4359b68e29d78a8bf7abca72973e9681
Jared Hensley <jahensle85@me.com>
2015-08-06 11:34:44 -0400

    basic functionality moving right

--- js/app.js
+++ js/app.js
@@ -1,48 +1,66 @@
 $(document).ready(function() {
 
-	var question1 = {
-		question: "Who discovered the contemporary model of DNA?",
-		choices: ["Gregor Mendel", "Friedrich Miescher", "Rosaland Franklin", "Watson and Crick"],
-		answer: ["Watson and Crick"],
-		completed: false
+	var questions = {
+		question: [["Who discovered the contemporary model of DNA?"],["Which of the following is not a nitrogenic base of DNA?"],["Which of the following is not a part of the central dogma of biology?"],["Who is generally credited with inventing the modern day microscope?"],["Which of the following is not considered one of the three domains of life?"]],
+		choices: [["Gregor Mendel", "Friedrich Miescher", "Rosaland Franklin", "Watson and Crick"],["Adenine", "Uracil", "Guanine", "Cytosine"],["Replication", "Transcription", "Translation", "Notsuration"],["Robert Hooke","Thomas Jefferson", "Benjamin Franklin", "Charles Darwin"],["Bactera","Eukaryota", "Archae", "Halophiles"]],
+		answer: [["Watson and Crick"],["Uracil"],["Notsuration"],["Robert Hooke"],["Halophiles"]],
+		completed: [false,false,false,false,false]
 	};
 
+	var totalScore = 0;
+	var globalCounter = 0;
 
-	var callQuestion = function () {
-		$("<div></div>").addClass("question").text(question1.question).appendTo(".inner-wrap");
+	var callQuestion = function (number) {
+		var j = number;
+		$("<div></div>").addClass("question").text(questions.question[j]).appendTo(".inner-wrap");
 		var populate = function() {
-			for (var i = 0; i < question1.choices.length; i++) {
-				$("<div>"+question1.choices[i]+"</div>").addClass("answer").appendTo(".inner-wrap");
+			globalCounter = number;
+			for (var i = 0; i < questions.choices[i].length; i++) {
+				$("<div>"+questions.choices[j][i]+"</div>").addClass("answer").appendTo(".inner-wrap");
 			}
+			return;
 		};
-
 		populate();
-
 	}
 
-	callQuestion();
+	var masterCallBack = function (number, callback) {
+		callback(number);
+	}
 
 
-	$(".answer").on("click", function() {
-		$('.select').removeClass('select');
-		$(this).toggleClass("select");
-		
-	});
 
-	var totalScore = 0;
+
+var style = function () {
+		$(".answer").on("click", function() {
+			$('.select').removeClass('select');
+			$(this).toggleClass("select");
+		});
+	}
+
+	masterCallBack(0, callQuestion);
+	style();
 
 	$(".bstyle2").on("click", function() {
-		
 		var check = $(".inner-wrap").find(".select").text();
-		if (check == question1.answer) {
+		if (check == questions.answer[globalCounter]) {
 			totalScore += 1;
 			$(".score").text(totalScore);
 		}
 
 	});
 
+
 	$(".arrowright").on("click", function() {
-		$(".inner-wrap").text("");
+		$(".inner-wrap").html("");
+		console.log(globalCounter);
+		if (globalCounter < 4) {
+			globalCounter += 1;
+			masterCallBack(globalCounter, callQuestion);
+			style();
+		} else {
+			$(".inner-wrap").html("Your score is " + totalScore);
+
+		}
 	});
 
 
